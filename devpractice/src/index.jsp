5eb0f2b43b1ad209e08a7f1b697e9471202149a8
chosc1796 <chosc1796@gmail.com>
2018-08-02 17:16:22 +0900

    1

---
+++ devpractice/src/index.jsp
@@ -0,0 +1,2 @@
+<link rel="shortcut icon" href="favicon.ico" />
+<meta http-equiv="Refresh" content="0; url=./asvc/common/commonLoginView.miaps?">
---
+++ devpractice/src/main/java/com/c/s/g/devpractice/DevpracticeApplication.java
@@ -0,0 +1,12 @@
+package com.c.s.g.devpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevpracticeApplication.class, args);
+	}
+}
